6dd0f3a7db84846286b5fa00b1c68bb2fa74387d
Johnspace153 <124692741+Johnspace153@users.noreply.github.com>
2024-10-11 16:58:18 +0200

    Add files via upload

---
+++ GuestBook1.2_App/GuestBook1.2/Program.cs
@@ -0,0 +1,111 @@
+﻿
+using GuestBookLibrary;
+
+internal class Program
+{
+    private static List<GuestModel> guestList = new List<GuestModel>();
+    private static void Main(string[] args)
+    {
+        WelcomeMessage();
+        GetGuestInfo();
+        PrintGuesttInfo();
+    }
+
+    private static void GetGuestInfo()
+    {
+        string anwser;
+        bool continueLoop = true;
+        
+        do
+        {
+            GuestModel guest = new GuestModel(RequestString("Please enter in your firstname: "),
+                                         RequestString("Please enter in your lastname: "),
+                                         RequestBirthday());
+
+            guest.MessageToHost = RequestString("What is your message to the host: ");
+
+            guestList.Add(guest);
+
+            anwser = RequestString("Is there more guests coming(Yes/No): ");
+
+            if (anwser.ToLower() == "no" )
+            {
+                Console.Clear();
+                Console.WriteLine("Enjoy your evening!");
+                Console.Write("\n");
+                continueLoop = false;
+            }
+            else
+            {
+                Console.Clear();
+            }
+        } while (continueLoop == true);
+        
+    }
+
+    private static void PrintGuesttInfo()
+    {
+        foreach (var guest in guestList)
+        {
+            Console.WriteLine($"{guest.FullName},{guest.Age} ,Message to Host: {guest.MessageToHost}");
+        }
+    }
+
+
+
+
+
+    private static void WelcomeMessage()
+    {
+        Console.WriteLine("Thank you for attending this splendid event!!");
+        Console.Write("\n");
+    }
+    private static string RequestString(string message)
+    {
+        Console.Write(message);
+        string output = Console.ReadLine();
+        Console.Write("\n");
+        return output;
+    }
+
+    private static DateTime RequestBirthday()
+    {
+        bool isValidDate = false;
+        string dateText;
+        DateTime output;
+
+        do
+        {
+            dateText = RequestString("Please enter in your birthday(yyyy-mm-dd): ");
+            isValidDate = DateTime.TryParse(dateText, out output);
+            Console.Write("\n");
+            if (isValidDate == false)
+            {
+                Console.WriteLine("Please enter the correct format!!");
+                Console.Write("\n");
+            }
+
+        } while (isValidDate == false);
+
+        return output;
+    }
+
+    private int RequestInt(string message) 
+    {
+        bool isValidNum = false;
+        int output;
+        
+        do
+        {
+            Console.Write(message);
+            string NumText = Console.ReadLine();
+            isValidNum = int.TryParse(NumText, out output);
+            if (isValidNum == false)
+            {
+                Console.WriteLine("Please type in valid number");
+            }
+
+        } while (isValidNum == false);
+        return output;
+    }
+}
---
+++ GuestBook1.2_App/GuestBookLibrary/GuestModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBookLibrary
+{
+    public class GuestModel
+    {
+        public string FirstName { private get; set; }
+        public string LastName { private get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public string MessageToHost { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Age => DateTime.Now.Year - BirthDate.Year;
+
+        public GuestModel(string firstName, string lastName, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+    }
+}
